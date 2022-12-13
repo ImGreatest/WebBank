3e9a146f09e1e6a928de128ef9fcb213985c30ea
ImGreatest <98634865+ImGreatest@users.noreply.github.com>
2022-12-13 14:44:18 +0300

    Delete polls directory

--- polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.create, name='home'),
-    path('mortgage', views.mortgage, name='mortgage'),
-    path('credit', views.credit, name='credit'),
-    path('deposit', views.deposit, name='deposit')
-]
